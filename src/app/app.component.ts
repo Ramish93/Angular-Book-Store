a1415e65093cabb9afe73835cabf37aac37ba4ee
ramishshah93 <ramishshah93@gmail.com>
2023-04-11 12:15:48 +0200

    created comp books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hey there</h1>',
+  templateUrl: './app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {}
