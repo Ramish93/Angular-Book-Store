1431b9387db2f8004a1f5c9e630b0cf689f2f388
ramishshah93 <ramishshah93@gmail.com>
2023-04-13 19:35:15 +0200

    register auth working with firebase

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterForm } from 'src/app/types/Auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
 
 @Component({
   selector: 'app-register',
@@ -12,4 +18,25 @@ export class RegisterComponent {
     password: '',
     confirmPassword: '',
   };
+  passwordMatched: boolean = true;
+
+  submit() {
+    if (
+      this.form.password !== this.form.confirmPassword ||
+      this.form.password === '' ||
+      this.form.confirmPassword === ''
+    ) {
+      this.passwordMatched = false;
+    }
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 }
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAzyw7VLX9RJy1EUx6vdHFErYrvhfmeOkI',
+  authDomain: 'bookstore-3cbf4.firebaseapp.com',
+  projectId: 'bookstore-3cbf4',
+  storageBucket: 'bookstore-3cbf4.appspot.com',
+  messagingSenderId: '25489566731',
+  appId: '1:25489566731:web:8bc58aa9d24c7ca7946b0e',
+};
