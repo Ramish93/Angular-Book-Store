02884ab5d1c728704e6f122b1f84268486ea0b2e
ramishshah93 <ramishshah93@gmail.com>
2023-04-13 11:48:29 +0200

    login page complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '', component: BooksComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
