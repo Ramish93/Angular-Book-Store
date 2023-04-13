90ba882c68f88195228228c48aa8d62bff401704
ramishshah93 <ramishshah93@gmail.com>
2023-04-13 10:59:58 +0200

    added angular routing for homepage-cartpage and tailwind for style

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+
+const routes: Routes = [{ path: '', component: BooksComponent }];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, CartComponent],
-  imports: [BrowserModule, FormsModule, BooksModule],
+  imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
